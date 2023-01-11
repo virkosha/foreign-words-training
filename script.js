9be2fb8df18e2a13f96b27334301bf567308f0be
Yana <virkosha@gmail.com>
2023-01-11 16:26:36 +0300

    Update script.js
    внесены измениения

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let studyMode = document.querySelector('#study-mode');
 let studyCards = document.querySelector('.study-cards');
 
+
 //переворот карточки по клику
 const flipCard = document.querySelector('.flip-card');
 flipCard.addEventListener('click', (event) => {
@@ -19,87 +20,88 @@ let exampleWordCotainer = cardBack.querySelector('span');
 let wordIndex = 0;
 
 const wordCards = [{
-            eng: 'apple',
-            rus: 'яблоко',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'pear',
-            rus: 'груша',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'ham',
-            rus: 'ветчина',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'cheese',
-            rus: 'сыр',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'cereals',
-            rus: 'злаки',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'cat',
-            rus: 'кот',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'dog',
-            rus: 'собака',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'game',
-            rus: 'игра',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'life',
-            rus: 'жизнь',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'love',
-            rus: 'любовь',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'pleasure',
-            rus: 'удовольствие',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'dream',
-            rus: 'мечта',
-            example: 'qwertyqwerty xxxx'
-        },
-        {
-            eng: 'wish',
-            rus: 'желание',
-            example: 'qwertyqwerty xxxx'
-        }
-    ]
-    //генерирую карточки в режиме "Тренировки" по порядку
-function learnWords() {
+        eng: 'apple',
+        rus: 'яблоко',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'pear',
+        rus: 'груша',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'ham',
+        rus: 'ветчина',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'cheese',
+        rus: 'сыр',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'cereals',
+        rus: 'злаки',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'cat',
+        rus: 'кот',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'dog',
+        rus: 'собака',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'game',
+        rus: 'игра',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'life',
+        rus: 'жизнь',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'love',
+        rus: 'любовь',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'pleasure',
+        rus: 'удовольствие',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'dream',
+        rus: 'мечта',
+        example: 'qwertyqwerty xxxx'
+    },
+    {
+        eng: 'wish',
+        rus: 'желание',
+        example: 'qwertyqwerty xxxx'
+    }
+]
 
+//генерирую карточки в режиме "Тренировки" по порядку
+function iterateArrayInOrder() {
     return wordCards[wordIndex];
 }
-
-//генерирую карточки в режиме "Тренировки" по кнопке "Перемешать слова"
+//генерирую карточки в режиме "Тренировки" рандомно по кнопке "Перемешать слова"
+function iterateArrayInRandom() {
+    wordCards.sort(() => Math.random() - 0.5);
+}
 const btnMixing = document.querySelector('#shuffle-words');
-btnMixing.addEventListener('click', shuffle(wordCards));
+btnMixing.addEventListener('click', iterateArrayInRandom());
 
-// вставляем первое слова в карточку
-let wordCurrent = learnWords();
-addWord();
+
+insertFollowingWords(wordIndex);
 //функция вставки последующих слов
-function addWord() {
-    wordCurrent = learnWords();
+function insertFollowingWords(wordIndex) {
+    wordCurrent = iterateArrayInOrder();
     let foreignWord = wordCurrent.eng;
     let translationWord = wordCurrent.rus;
     let exampleWord = wordCurrent.example;
@@ -108,6 +110,7 @@ function addWord() {
     translationWordContainer.textContent = translationWord;
     exampleWordCotainer.textContent = exampleWord;
 }
+
 // листать слова с режиме Тренировка
 const arrowBack = document.querySelector('#back');
 const arrowNext = document.querySelector('#next');
@@ -115,7 +118,7 @@ const arrowNext = document.querySelector('#next');
 arrowBack.addEventListener('click', (event) => {
     //покажи элемент массива-1  *индекс карточки
     wordIndex--;
-    addWord();
+    insertFollowingWords();
     wordNumber.textContent = --wordNumber.textContent;
     blockingArrow(wordNumber.textContent);
 
@@ -125,7 +128,7 @@ arrowBack.addEventListener('click', (event) => {
 arrowNext.addEventListener('click', (event) => {
     //покажи элемент массива+1     *индекс карточки
     wordIndex++;
-    addWord();
+    insertFollowingWords();
     wordNumber.textContent = ++wordNumber.textContent;
     blockingArrow(wordNumber.textContent);
 
@@ -159,20 +162,52 @@ function blockingArrow() {
     }
 }
 
+/*
 //процент просмотренных слов
 let percent = document.querySelector('#words-progress');
-percent.textContent = `${totalNumberContainer.textContent * 100}%`;
+percent.value = `${totalNumberContainer.textContent * 100 % 13}`;
+*/
 
 //переключение режима
 const btnExam = document.querySelector('#exam');
+//режим "Проверки знаний"
+let examMode = document.querySelector('#exam-mode');
+
+let listEng = [];
+let listRus = [];
+let examCards = document.querySelector('#exam-cards');
+
+
+//расположение карточек в рандомном порядке в режиме "Проверки знаний"
+function spreadOutCardsInExamMode() {
+    wordCards.forEach((elem) => {
+        listEng.push(elem.eng);
+        listRus.push(elem.rus);
+    })
+    let cardExam = document.createElement('div');
+    cardExam.classList.add('card');
+    listEng.forEach((elem) => {
+        cardExam.textContent = elem;
+    })
+    listRus.forEach((elem) => {
+        cardExam.textContent = elem;
+    })
+
+    examCards.prepend(cardExam);
+    return examCards;
+}
+
+console.log(listEng);
+console.log(listRus);
 
 btnExam.addEventListener('click', (event) => {
     examMode.classList.toggle('hidden');
     studyMode.classList.toggle('hidden');
     studyCards.classList.toggle('hidden');
-    makeRandomCards();
+    spreadOutCardsInExamMode();
 
-    /*  timeId = setInterval(() => {
+    /* 
+     timeId = setInterval(() => {
           let [minutes, seconds] = time.textContent.split(':').map(Number);
 
           if (seconds < 59) {
@@ -183,107 +218,5 @@ btnExam.addEventListener('click', (event) => {
           }
 
           time.textContent = `${format(minutes)}:${format(seconds)}`;
-      }, 1000)*/
+      }, 1000);*/
 })
-
-//функция перемешивания
-function shuffle(arr) {
-    arr.sort(() => Math.random() - 0.5);
-}
-
-//режим "Проверки знаний"
-let examMode = document.querySelector('#exam-mode');
-
-
-//место вставки для карточек слов
-let examCards = document.querySelector('#exam-cards');
-let cardExam;
-let examCardsWord;
-
-//расположение карточек в случайном порядке
-function makeRandomCards() {
-    //собираем слова в массив
-    const words = [
-        ...wordCards.map((item) => item.eng),
-        ...wordCards.map((item) => item.rus)
-    ];
-    shuffle(words);
-
-    //создание шаблонов карточек и их заполнение
-    for (wordIndex = 0; wordIndex < words.length; wordIndex++) {
-        cardExam = document.createElement('div');
-        cardExam.classList.add('card');
-        examCards.prepend(cardExam);
-        examCardsWord = document.createElement('p');
-        examCardsWord.textContent = words[wordIndex];
-        cardExam.prepend(examCardsWord);
-
-        cardExam.addEventListener('click', () => {
-            cardExam.classList.add('correct');
-        })
-
-    }
-}
-//логика игры
-/*
-const highlighting = setTimeout(() => {
-    twoCard.classList.remove('wrong');
-    firstCard.classList.remove('correct');
-}, 500); */
-
-cardExam.addEventListener('click', () => {
-    if (wordCards.findIndex(firstCard) == wordCards.findIndex(twoCard)) {
-        twoCard.classList.add('correct');
-        firstCard.classList.add('fade-out');
-        twoCard.classList.add('fade-out');
-    } else {
-        twoCard.classList.add('wrong');
-    }
-
-    const highlighting = setTimeout(() => {
-        twoCard.classList.remove('wrong');
-        firstCard.classList.remove('correct');
-    }, 500);
-})
-
-const dictionary = {
-    apple: "яблоко",
-    pear: "груша",
-    ham: "ветчина",
-    cheese: "сыр",
-    cereals: "злаки",
-    cat: "кот",
-    dog: "собака",
-    game: "игра",
-    life: "жизнь",
-    love: "любовь",
-    pleasure: "удовольствие",
-    dream: "мечта",
-    wish: "желание"
-
-}
-
-//модальное окно о завершении игры
-function finish() {
-    if (wordCards.querySelector('.card') == null) {
-        alert('Проверка знаний завершена');
-    }
-}
-
-
-
-//при совпадении удалять карты
-
-/*
-//отображение времени тестирования
-const time = document.querySelector('#time');
-//получаем доступ к таймеру
-let timeId;
-
-//описываем формат времени таймера
-function format(val) {
-    if (val < 10) {
-        return `0${val}`;
-    }
-    return val;
-}*/
